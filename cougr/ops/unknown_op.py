c7147a2b3cc0ab5d82523ef27db84075c2bf1ce4
Joel Hestness <hestnessjoel@baidu.com>
2018-06-03 13:10:11 -0700

    Let UnknownOp pass propagateShapes with warning

--- cougr/ops/unknown_op.py
+++ cougr/ops/unknown_op.py
@@ -7,6 +7,12 @@ class UnknownOp(Op):
     def __init__(self, name):
         super(UnknownOp, self).__init__(name)
 
+    def propagateShapes(self):
+        if not UnknownOp._warned_once:
+            print('WARN: Graph contains unknown ops. ' \
+                  'Not sure how to propagate shapes!')
+            UnknownOp._warned_once = True
+
     def calcAlgFlops(self):
         if not UnknownOp._warned_once:
             print('WARN: Graph contains unknown ops. ' \
