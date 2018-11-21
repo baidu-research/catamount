00ecd87799d010b06e6c3043dda24f9e7b5ecd58
Joel Hestness <hestnessjoel@baidu.com>
2018-11-21 11:02:08 -0800

    Update setup.py for prime time (#15)

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='cougr',
-   version='0.9',
-   description='CouGr: Compute Graph Analyzer',
-   author='CouGr Developers',
-   author_email='joel@baidu.com',
-   packages=find_packages(),
-   install_requires=[],
+    name='cougr',
+    version='0.9',
+    description='CouGr: Compute Graph Analyzer',
+    author='CouGr Developers',
+    author_email='joel@baidu.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'sympy',
+        'tensorflow>=1.7',
+    ],
 )
