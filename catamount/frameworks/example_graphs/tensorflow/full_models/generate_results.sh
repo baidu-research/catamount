24d28e4a22d0477780b3cb2091c94fab2ffcbef8
Joel Hestness <hestnessjoel@baidu.com>
2018-11-23 10:38:05 -0800

    Final additions to fully generate and gather PPoPP 2019 results

--- catamount/frameworks/example_graphs/tensorflow/full_models/generate_results.sh
+++ catamount/frameworks/example_graphs/tensorflow/full_models/generate_results.sh
@@ -21,26 +21,36 @@ echo "==== Installing Catamount ===="
 python setup.py install
 echo ""
 
+outdir="ppopp_2019_tests"
+if [ ! -e "$outdir" ]
+then
+	echo "==== Creating output directory ===="
+	mkdir -p $outdir
+	echo ""
+fi
 
 # Run tests
 # Use CUDA_VISIBLE_DEVICES="" in case system has access to GPUs,
 # but we don't want to use them
 for depth in 18 34 50 101 152
 do
-	echo "==== Running Image ResNet$depth. Output to output_image_$depth.txt ===="
-	CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_image_resnet.py --depth $depth >& output_image_$depth.txt
+	outfile="$outdir/output_image_$depth.txt"
+	echo "==== Running Image ResNet$depth. Output to $outfile ===="
+	CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_image_resnet.py --depth $depth >& $outfile
 	echo ""
 done
 
 for domain in charlm nmt wordlm
 do
-	echo "==== Running Language $domain. Output to output_$domain.txt ===="
-	CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_language_models.py --domain $domain >& output_$domain.txt
+	outfile="$outdir/output_$domain.txt"
+	echo "==== Running Language $domain. Output to $outfile ===="
+	CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_language_models.py --domain $domain >& $outfile
 	echo ""
 done
 
-echo "==== Running Speech Attention. Output to output_speech.txt ===="
-CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_speech_attention.py >& output_speech.txt
+outfile="$outdir/output_speech.txt"
+echo "==== Running Speech Attention. Output to $outfile ===="
+CUDA_VISIBLE_DEVICES="" python catamount/tests/full/tf_speech_attention.py >& $outfile
 echo ""
 
 
