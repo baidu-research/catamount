2aea94b10d2910a9c24ee659639f25a178f9e1c4
Joel Hestness <joel@cerebras.net>
2019-07-31 18:53:40 -0700

    (Weird) Adjustments for speech attention model, and suppress pytest warnings

--- catamount/tests/run_tests.sh
+++ catamount/tests/run_tests.sh
@@ -5,19 +5,19 @@ echo -e "------------ Running ops tests ---------------\n\n"
 for testfile in `ls catamount/tests/ops/*.py`
 do
 	echo Running test: $testfile
-	python -m pytest $testfile
+	python -m pytest $testfile -p no:warnings
 done
 
 echo -e "\n\n------------ Running API tests ---------------\n\n"
 for testfile in `ls catamount/tests/api/*.py`
 do
 	echo Running test: $testfile
-	python -m pytest $testfile
+	python -m pytest $testfile -p no:warnings
 done
 
 echo -e "\n\n------------ Running full tests ---------------\n\n"
 for testfile in `ls catamount/tests/full/*.py`
 do
 	echo Running test: $testfile
-	python -m pytest $testfile
+	python -m pytest $testfile -p no:warnings
 done
