6cc751623ff1fa25005317af450c140201756af8
Joel Hestness <joel@cerebras.net>
2019-07-31 19:13:24 -0700

    Make tests script spit out a nice summary

--- catamount/tests/run_tests.sh
+++ catamount/tests/run_tests.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+testsfailed="0"
+failedtests=()
+testspassed="0"
 
 echo -e "------------ Running ops tests ---------------\n\n"
 for testfile in `ls catamount/tests/ops/*.py`
 do
 	echo Running test: $testfile
 	python -m pytest $testfile -p no:warnings
+	if [ "$?" != "0" ]
+	then
+		failedtests[$testsfailed]="$testfile"
+		testsfailed=$(($testsfailed + 1))
+	else
+		testspassed=$(($testspassed + 1))
+	fi
 done
 
 echo -e "\n\n------------ Running API tests ---------------\n\n"
@@ -13,6 +23,13 @@ for testfile in `ls catamount/tests/api/*.py`
 do
 	echo Running test: $testfile
 	python -m pytest $testfile -p no:warnings
+	if [ "$?" != "0" ]
+	then
+		failedtests[$testsfailed]="$testfile"
+		testsfailed=$(($testsfailed + 1))
+	else
+		testspassed=$(($testspassed + 1))
+	fi
 done
 
 echo -e "\n\n------------ Running full tests ---------------\n\n"
@@ -20,4 +37,20 @@ for testfile in `ls catamount/tests/full/*.py`
 do
 	echo Running test: $testfile
 	python -m pytest $testfile -p no:warnings
+	if [ "$?" != "0" ]
+	then
+		failedtests[$testsfailed]="$testfile"
+		testsfailed=$(($testsfailed + 1))
+	else
+		testspassed=$(($testspassed + 1))
+	fi
+done
+
+echo -e "\n\n------------ Tests summary ---------------\n\n"
+totaltests=$(($testsfailed + $testspassed))
+echo "Tests passed: $testspassed"
+echo "Tests failed: $testsfailed"
+for i in "${failedtests[@]}"
+do
+	echo "        $i"
 done
