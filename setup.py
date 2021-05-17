0bd94b5d9bfc26fe03503c61a1633dc0d705fbc7
Joel Hestness <jthestness@gmail.com>
2021-05-17 13:05:54 -0700

    setup.py: Add optional TF-gpu constraint

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import setup, find_packages
+
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
+
+install_deps = ['numpy', 'sympy']
+
+if get_dist('tensorflow') is None and get_dist('tensorflow_gpu') is None:
+    install_deps.append('tensorflow')
 
 setup(
     name='catamount',
@@ -7,9 +20,5 @@ setup(
     author='Catamount Developers',
     author_email='joel@baidu.com',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'sympy',
-        'tensorflow>=1.7',
-    ],
+    install_requires=install_deps,
 )
